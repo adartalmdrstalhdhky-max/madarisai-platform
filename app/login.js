a376bd476b1bc6537fec6c37b67a650148937fe7
عصام عبدالقوي احمد صالح <adartalmdrstalhdhky@gmail.com>
2026-04-09 19:15:12 +0300

    Update login.js

--- app/login.js
+++ app/login.js
@@ -1,4 +1,4 @@
-تimport { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
 import {
   getAuth,
   signInWithEmailAndPassword,
