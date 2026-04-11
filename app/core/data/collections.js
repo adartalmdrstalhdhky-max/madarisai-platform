bf090dcc029fec4679cc45bdd80f5b234bc392f4
عصام عبدالقوي احمد صالح <adartalmdrstalhdhky@gmail.com>
2026-04-11 15:31:46 +0300

    Create collections.js

---
+++ app/core/data/collections.js
@@ -0,0 +1,11 @@
+// app/core/data/collections.js
+import { COLLECTIONS } from "./schema-contract.js";
+
+export const schoolsCol = COLLECTIONS.schools;
+export const usersCol = COLLECTIONS.users;
+export const teachersCol = COLLECTIONS.teachers;
+export const studentsCol = COLLECTIONS.students;
+export const classesCol = COLLECTIONS.classes;
+export const subjectsCol = COLLECTIONS.subjects;
+export const classSubjectsCol = COLLECTIONS.classSubjects;
+export const schedulesCol = COLLECTIONS.schedules;
